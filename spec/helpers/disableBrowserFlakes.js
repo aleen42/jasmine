936d8f8b91e0a7b9a2f8079672be1b30457f0b10
Steve Gravrock <sdg@panix.com>
2021-09-20 16:07:17 -0700

    Always run browser flakes

--- spec/helpers/disableBrowserFlakes.js
+++ spec/helpers/disableBrowserFlakes.js
@@ -1,7 +1,7 @@
 (function(env) {
   env.skipBrowserFlake = function() {
-    pending(
-      'Skipping specs that are known to be flaky in browsers in this run'
-    );
+    // pending(
+    //   'Skipping specs that are known to be flaky in browsers in this run'
+    // );
   };
 })(jasmine.getEnv());
